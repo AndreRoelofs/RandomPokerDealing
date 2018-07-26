5ab7eedbe111d33f51d435210cc1d15e079e54a9
Andre <p.arroelofs@gmail.com>
2018-07-26 17:01:55 +0200

    ejected

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Header from './app/components/Header/Header';
-import PokerIndex from './app/components/Poker/Poker-Index/Poker-Index';
-import './App.css';
+import logo from './logo.svg';
+import './App.scss';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Router>
-        <div>
-          <Route exact path="/" component={PokerIndex} />
-        </div>
-      </Router>
-    </div>
 
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">
+Welcome to React
+      </h1>
+    </header>
+    <p className="App-intro">
+        To get started, edit
+      {' '}
+      <code>
+src/App.js
+      </code>
+      {' '}
+and save to reload.
+    </p>
+  </div>
+);
+
 
 export default App;
-// <div className="App">
-//   <Header />
-//   <header className="App-header">
-//     <h1 className="App-title">
-//       Welcome to React
-//     </h1>
-//   </header>
-// </div>
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,9 @@
-/* eslint-disable no-undef */
-
 import React from 'react';
-import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-
 it('renders without crashing', () => {
-  const component = renderer.create(<App />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
